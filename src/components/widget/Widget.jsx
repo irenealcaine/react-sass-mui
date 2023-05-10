d5a53b0f095e7a5bc173f253ad5bcda2b7435b32
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-05-10 11:17:33 +0000

    Update Widget

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,10 +1,22 @@
 import "./widget.scss";
+import KeyboardArrowUpOutlinedIcon from "@mui/icons-material/KeyboardArrowUpOutlined";
+import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 
 const Widget = () => {
   return (
     <div className="widget">
-      <div className="left">left</div>
-      <div className="right">right</div>
+      <div className="left">
+        <span className="title">USERS</span>
+        <span className="counter">214</span>
+        <span className="link">See more</span>
+      </div>
+      <div className="right">
+        <div className="percentage positive">
+          <KeyboardArrowUpOutlinedIcon />
+          20%
+        </div>
+        <AccountCircleOutlinedIcon className="icon" />
+      </div>
     </div>
   );
 };
