deffbd45104ebf679df1ef62d838bb135daf78db
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-05-11 07:30:59 +0000

    Update Featured

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -2,6 +2,7 @@ import "./featured.scss";
 import DragIndicatorIcon from "@mui/icons-material/DragIndicator";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 
 const Featured = () => {
   return (
@@ -10,14 +11,41 @@ const Featured = () => {
         <h1 className="title">Total revenue</h1>
         <DragIndicatorIcon fontSize="small" />
       </span>
-      <span className="bottom">
+      <div className="bottom">
         <div className="featuredChart">
           <CircularProgressbar value={70} text={"70%"} strokeWidth={3} />
         </div>
         <p className="title">Total sales made today</p>
         <p className="amount">420 €</p>
-        <p className="desc">Only last month</p>
-      </span>
+        <p className="desc">
+          Previous transactions processing. Lasta payments may not be included.
+        </p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult">
+              <ArrowDropDownIcon fontSize={"small"} />
+              <div className="resultAmount">12.4k €</div>
+            </div>
+          </div>
+
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult">
+              <ArrowDropDownIcon fontSize={"small"} />
+              <div className="resultAmount">12.4k €</div>
+            </div>
+          </div>
+
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult">
+              <ArrowDropDownIcon fontSize={"small"} />
+              <div className="resultAmount">12.4k €</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
