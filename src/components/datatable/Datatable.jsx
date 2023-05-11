5cc4ca63f629fd9087acf20b98bd706f9c262277
Irene Alcaine <irenealcainealvarez@gmail.com>
2023-05-11 19:51:13 +0200

    update list

---
+++ src/components/datatable/Datatable.jsx
@@ -0,0 +1,25 @@
+import "./datatable.scss";
+import { DataGrid } from "@mui/x-data-grid";
+import { userColumns, userRows } from "../../datatablesource";
+
+
+
+const Datatable = () => {
+  return (
+    <div className='datatable'>
+      <DataGrid
+        rows={userRows}
+        columns={userColumns}
+        initialState={{
+          pagination: {
+            paginationModel: { page: 0, pageSize: 5 },
+          },
+        }}
+        pageSizeOptions={[5, 10]}
+        checkboxSelection
+      />
+    </div>
+  )
+}
+
+export default Datatable
