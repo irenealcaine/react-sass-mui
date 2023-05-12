d9c1bafbc828d35572a5cc0c233773ff200779e2
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-05-12 11:24:47 +0000

    Update sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -13,11 +13,16 @@ import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
 import AccountBoxIcon from "@mui/icons-material/AccountBox";
 import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 
+import { Link } from "react-router-dom";
+// import { DashboardIcon } from "@mui/icons-material";
+
 const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="top">
-        <span className="logo">Irene</span>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <span className="logo">Irene</span>
+        </Link>
       </div>
       <hr />
       <div className="center">
@@ -28,14 +33,18 @@ const Sidebar = () => {
             <span>Dashboard</span>
           </li>
           <p className="title">LISTS</p>
-          <li>
-            <PersonIcon className="icon" />
-            <span>Users</span>
-          </li>
-          <li>
-            <Inventory2OutlinedIcon className="icon" />
-            <span>Products</span>
-          </li>
+          <Link to="/users" style={{ textDecoration: "none" }}>
+            <li>
+              <PersonIcon className="icon" />
+              <span>Users</span>
+            </li>
+          </Link>
+          <Link to="/products" style={{ textDecoration: "none" }}>
+            <li>
+              <Inventory2OutlinedIcon className="icon" />
+              <span>Products</span>
+            </li>
+          </Link>
           <li>
             <InventoryOutlinedIcon className="icon" />
             <span>Orders</span>
