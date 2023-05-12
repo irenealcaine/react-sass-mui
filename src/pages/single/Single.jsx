b46ba58073c6b516a3df1f821bd4956df89e4a89
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-05-12 08:05:33 +0000

    Update Single

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -3,6 +3,7 @@ import "./single.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import Chart from "../../components/chart/Chart";
+import List from "../../components/table/Table";
 
 const Single = () => {
   return (
@@ -42,10 +43,13 @@ const Single = () => {
             </div>
           </div>
           <div className="right">
-            <Chart aspect={3 / 1} title="User spending (Last 6 months)"/>
+            <Chart aspect={3 / 1} title="User spending (Last 6 months)" />
           </div>
         </div>
-        <div className="bottom"></div>
+        <div className="bottom">
+          <h1 className="title">Last transactions</h1>
+          <List />
+        </div>
       </div>
     </div>
   );
