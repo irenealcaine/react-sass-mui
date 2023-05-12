25c26c68a09d5178a78743f56500fe214e071fa7
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-05-12 11:06:03 +0000

    Update new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/home/Home";
-import Login from "./pages/login/Login.jsx"
-import List from "./pages/list/List.jsx"
-import Single from "./pages/single/Single.jsx"
-import New from "./pages/new/New.jsx"
+import Login from "./pages/login/Login.jsx";
+import List from "./pages/list/List.jsx";
+import Single from "./pages/single/Single.jsx";
+import New from "./pages/new/New.jsx";
+import { userInputs, productInputs } from "./formSource";
 
 function App() {
   return (
@@ -14,15 +15,21 @@ function App() {
           <Route path="/">
             <Route index element={<Home />} />
             <Route path="login" element={<Login />} />
-            <Route path="users" >
+            <Route path="users">
               <Route index element={<List />} />
               <Route path=":userId" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route
+                path="new"
+                element={<New inputs={userInputs} title="Add new user" />}
+              />
             </Route>
-            <Route path="products" >
+            <Route path="products">
               <Route index element={<List />} />
               <Route path=":productId" element={<Single />} />
-              <Route path="new" element={<New />} />
+              <Route
+                path="new"
+                element={<New inputs={productInputs} title="Add new poduct" />}
+              />
             </Route>
           </Route>
         </Routes>
