7e07a673e7c5b9ee9b07694264ab64c2d6cb4c44
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-05-12 06:34:23 +0000

    Update Datatable

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -2,24 +2,38 @@ import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
 import { userColumns, userRows } from "../../datatablesource";
 
-
-
 const Datatable = () => {
+  const actionColumn = [
+    {
+      feld: "action",
+      heaerName: "Action",
+      width: 200,
+      renderCell: () => {
+        return (
+          <div className="cellAction">
+            <div className="viewButton">View</div>
+            <div className="deleteButton">Delete</div>
+          </div>
+        );
+      },
+    },
+  ];
+
   return (
-    <div className='datatable'>
+    <div className="datatable">
       <DataGrid
         rows={userRows}
-        columns={userColumns}
+        columns={userColumns.concat(actionColumn)}
         initialState={{
           pagination: {
-            paginationModel: { page: 0, pageSize: 5 },
+            paginationModel: { page: 0, pageSize: 9 },
           },
         }}
-        pageSizeOptions={[5, 10]}
+        pageSizeOptions={[9, 18]}
         checkboxSelection
       />
     </div>
-  )
-}
+  );
+};
 
-export default Datatable
+export default Datatable;
