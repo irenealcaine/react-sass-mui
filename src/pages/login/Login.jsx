6c720aa10f9bb548a3b6e61968dc67dcca1b2e2e
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-05-15 08:28:08 +0000

    Update firebase auth

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,21 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
 import "./login.scss";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../../firebase";
+import { useNavigate } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
 
 const Login = () => {
   const [error, setError] = useState(false);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
+  const navigate = useNavigate();
+
+  const { dispatch } = useContext(AuthContext);
+
   const handleLogin = (e) => {
     e.preventDefault();
 
-    createUserWithEmailAndPassword(auth, email, password)
+    signInWithEmailAndPassword(auth, email, password)
       .then((userCredential) => {
         // Signed in
         const user = userCredential.user;
-        console.log(user);
+        dispatch({ type: "LOGIN", payload: user });
+        navigate("/");
+
         // ...
       })
       .catch((error) => {
