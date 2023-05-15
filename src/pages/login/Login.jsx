51955ff88a18b5bc15884e3b56c0333b74982cb8
ialcaine <101673351+irenealcaine@users.noreply.github.com>
2023-05-15 07:21:20 +0000

    Update firebase.js and Login.jsx

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,24 +1,47 @@
-import React, { useState } from 'react'
-import "./login.scss"
+import React, { useState } from "react";
+import "./login.scss";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../../firebase";
 
 const Login = () => {
-
-  const [error, setError] = useState(false)
+  const [error, setError] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleLogin = (e) => {
-    e.preventDefault()
-  }
+    e.preventDefault();
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        // ...
+      })
+      .catch((error) => {
+        setError(true);
+        // ..
+      });
+  };
 
   return (
-    <div className='login'>
+    <div className="login">
       <form onSubmit={handleLogin}>
-        <input type="email" placeholder='email' />
-        <input type="password" placeholder='password' />
-        <button type='submit'>Login</button>
+        <input
+          type="email"
+          placeholder="email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit">Login</button>
         {error && <span>Wrong mail or password!</span>}
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
